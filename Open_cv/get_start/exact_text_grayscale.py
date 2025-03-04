3eadb9300bf8bf459cf49172b1c49290d19a7752
1ConHeoUnIn <SkullCaptainp@gmail.com>
2025-03-04 15:23:46 +0700

    update

--- Open_cv/get_start/exact_text_grayscale.py
+++ Open_cv/get_start/exact_text_grayscale.py
@@ -1,19 +1,23 @@
 # read image
 import cv2
-
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 # Đường dẫn đến hình ảnh
-image_path = r'D:\Test_Lab\Open_cv\photos\hoa.jpg'  # Sử dụng r'' để tránh lỗi escape
+image_path = r'D:\Test_Lab\Open_cv\photos\sample1.jpg'  # Sử dụng r'' để tránh lỗi escape
 
 # Đọc hình ảnh từ file
 image = cv2.imread(image_path)
-
+grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Kiểm tra xem hình ảnh có được đọc thành công không
 if image is None:
     print("Error: Không thể đọc hình ảnh. Vui lòng kiểm tra đường dẫn.")
 else:
     # Hiển thị hình ảnh
     cv2.imshow('Image', image)
-
+    cv2.imshow('grayscale_image', grayscale_image)
+    text = pytesseract.image_to_string(grayscale_image, lang='vie')
+    print(text)
     # Chờ người dùng nhấn phím và đóng cửa sổ
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
