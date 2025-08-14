bda261df1b757e60343301edba4b3f6036d36b2d
1ConHeoUnIn <skullcaptainp@gmail.com>
2025-08-14 13:54:38 +0700

    storage

--- Esp32/test_servo/src/main.cpp
+++ Esp32/test_servo/src/main.cpp
@@ -1,23 +1,23 @@
 #include <Arduino.h>
-#include <ESP32Servo.h>
-Servo myservo;
 
-int servoPin =18;
+const int servoPin = 2;
+
+uint32_t pulseToDuty(int microseconds); // Khai báo prototype
 
 void setup() {
-myservo.attach(servoPin);
-Serial.begin(115200);
+  ledcSetup(0, 50, 16);           // Tần số 50Hz, độ phân giải 16 bit
+  ledcAttachPin(servoPin, 0);
 }
 
 void loop() {
-  if(Serial.available()){
-    int angle=Serial.parseInt();
-    myservo.write(angle);
-  }
-  Serial.print("Angle:");
-  Serial.println(myservo.read());
-  delay(20);
+  ledcWrite(0, pulseToDuty(500));   // Góc 0°
+  delay(1000);
+  ledcWrite(0, pulseToDuty(1500));  // Góc 90°
+  delay(1000);
+  ledcWrite(0, pulseToDuty(2400));  // Góc 180°
+  delay(1000);
 }
 
-
-
+uint32_t pulseToDuty(int microseconds) {
+  return (uint32_t)((microseconds * 65536L) / 20000L);
+}
